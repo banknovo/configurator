a3e453b194b0fa3f76e99dd17ad3db7a131d6ba6
Dhwaneet Bhatt <dhwaneet@banknovo.com>
2020-05-04 17:42:36 +0530

    Refactoring to support more cli modes

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -21,3 +21,20 @@ func TestGetEnvironment(t *testing.T) {
 		require.Equal(t, test.expected, actual)
 	}
 }
+
+func TestRemovePrefix(t *testing.T) {
+	tests := []struct {
+		key          string
+		prefixLength int
+		expected     string
+	}{
+		{"/Key1/Key2/Key3/Key4", 0, "Key1/Key2/Key3/Key4"},
+		{"/Key1/Key2/Key3/Key4", 1, "Key2/Key3/Key4"},
+		{"/Key1/Key2/Key3/Key4", 2, "Key3/Key4"},
+		{"/Key1/Key2/Key3/Key4", 3, "Key4"},
+	}
+	for _, test := range tests {
+		actual := removePrefix(test.key, test.prefixLength)
+		require.Equal(t, test.expected, actual)
+	}
+}
